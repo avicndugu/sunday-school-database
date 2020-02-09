55bd763492802a7278451704a31befaa8b7db931
avicndugu <avicnduguh@gmail.com>
2020-02-10 00:02:30 +0300

    Update classview data in table

--- ssdatabase/expressapp/routes/classview.js
+++ ssdatabase/expressapp/routes/classview.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var connection  = require('../lib/db');
+var data;
+
+/* GET users listing. */
+connection.query('SELECT * FROM employees',(err,rows) => {
+    if (err) throw err;
+    console.log('Data received!');
+    data=rows;
+    console.log(data);
+});
 
 /* GET login file. */
 router.get('/', function(req, res) {
-  res.render('classview');
+  res.render('classview', {arrays:data});
 });
 
 module.exports = router;
