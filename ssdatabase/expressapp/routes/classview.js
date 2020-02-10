093b91934c92c171eca0f2c69b3e7a7a2708e904
avicndugu <avicnduguh@gmail.com>
2020-02-10 14:20:27 +0300

    Added insert query under the get request

--- ssdatabase/expressapp/routes/classview.js
+++ ssdatabase/expressapp/routes/classview.js
@@ -4,16 +4,16 @@ var connection  = require('../lib/db');
 var data;
 
 /* GET users listing. */
-connection.query('SELECT * FROM employees',(err,rows) => {
-    if (err) throw err;
-    console.log('Data received!');
-    data=rows;
-    console.log(data);
-});
+
 
 /* GET login file. */
 router.get('/', function(req, res) {
-  res.render('classview', {arrays:data});
+  connection.query('SELECT * FROM employees',(err,rows) => {
+    if (err) throw err;
+    console.log('Data received!');
+    data=rows;
+    res.render('classview', {arrays:data});
+  });
 });
 
 module.exports = router;
